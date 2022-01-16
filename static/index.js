20141089b5d88d3ef585aa69b47f6dab30c045cf
Saitore973 <saitorevg@gmail.com>
2022-01-16 19:53:42 +0300

    create a calculate price function

--- static/index.js
+++ static/index.js
@@ -1,6 +1,20 @@
+function calculatePrice(){
 let stuffed = document.getElementById('stuff');
 let thin = document.getElementById("thin");
 let cracker = document.getElementById("crack");
 let perperoni = document.getElementById("perp");
 let mayonaise = document.getElementById("mayo");
 let vegeterian = document.getElementById("vegan");
+ let itemList = document.querySelector("#select").value;
+ let price = document.querySelector("#price");
+ let quantity = document.getElementById("quantity").value;
+
+ let prices = [500, 800,1200];
+ let crust = {
+   thin: 200,
+   stuffed: 350,
+   cracker: 300,
+
+ };
+
+ 
