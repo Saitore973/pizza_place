ec640c83c0f4d994a6f958a1be29e9a4132dfd65
Saitore973 <saitorevg@gmail.com>
2022-01-16 18:35:04 +0300

    declare the crust and topping variables

---
+++ static/index.js
@@ -0,0 +1,6 @@
+let stuffed = document.getElementById('stuff');
+let thin = document.getElementById("thin");
+let cracker = document.getElementById("crack");
+let perperoni = document.getElementById("perp");
+let mayonaise = document.getElementById("mayo");
+let vegeterian = document.getElementById("vegan");
